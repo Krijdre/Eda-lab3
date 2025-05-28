533815fb0be18fc4bb0b62163507daa112db9057
fringo <nicolas.navarro2@mail.udp.cl>
2025-05-28 01:03:50 -0400

    BubbleSortish

--- codigo.java
+++ codigo.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 class Game{
     private String name;
@@ -29,8 +28,8 @@ class Game{
 }
 
 class GenerateData{
-    String[] palabras = {"Dragon", "Empire", "Quest", "Galaxy", "Legends", "Warrior"}
-    String[] categorias = {"Acción", "Aventura", "Estrategia", "RPG", "Deportes", "Simulación"}
+    String[] palabras = {"Dragon", "Empire", "Quest", "Galaxy", "Legends", "Warrior"};
+    String[] categorias = {"Acción", "Aventura", "Estrategia", "RPG", "Deportes", "Simulación"};
     Random randin = new Random();
 
     public ArrayList<Game> GenerateData(int NumeroJ){
@@ -47,140 +46,177 @@ class GenerateData{
 
 }
 
-class Dataset{
+class Dataset {
     private ArrayList<Game> data;
     private String sortedByAttribute;
 
-    public Dataset(ArrayList<Game> data){
+    public Dataset(ArrayList<Game> data) {
         this.data = new ArrayList<>(data);
         this.sortedByAttribute = "none";
     }
 
-        private int BusquedaporPrecio(int elprecioabuscar){
-            int izquierda = 0;
-            int derecha = data.size()-1;
-            while (izquierda <= derecha){
-                int centro = izquierda + (derecha - izquierda)/2;
-                int actual = data.get(centro).getPrice();
-
-                if (actual == elprecioabuscar){
-                    return centro;
-                }else if (actual > elprecioabuscar){
-                    derecha = centro-1;
-                }else{
-                    izquierda = centro+1;
+    public void bubbleSortByPrice() {
+        int n = data.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (data.get(j).getPrice() > data.get(j + 1).getPrice()) {
+                    swap(j, j + 1);
                 }
             }
-            return -1; // no se encuentra en la lista
         }
-
-        private int BusquedaporCategoria(String lacategoria){
-        int izquierda = 0;
-        int derecha = data.size()-1;
-        while (izquierda <= derecha){
-            int centro = izquierda + (derecha - izquierda)/2;
-            int actual = data.get(centro).getCategory().compareTo(lacategoria);
-            if(actual == 0){
-                return centro;
-            }else if(actual < 0){
-                izquierda = centro+1;
-            }else{
-                derecha = centro-1;
-            }
-        }
-            return -1;
+        sortedByAttribute = "price";
     }
 
-    private int BusquedaporCalidad(int lacalidad){
-        int izquierda = 0;
-        int derecha = data.size()-1;
-        while (izquierda <= derecha){
-            int centro = izquierda + (derecha - izquierda)/2;
-            int actual = data.get(centro).getQuality();
+    public void bubbleSortByCategory() {
+        int n = data.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (data.get(j).getCategory().compareTo(data.get(j + 1).getCategory()) > 0) {
+                    swap(j, j + 1);
+                }
+            }
+        }
+        sortedByAttribute = "category";
+    }
 
-            if(actual == lacalidad){
+    public void bubbleSortByQuality() {
+        int n = data.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (data.get(j).getQuality() > data.get(j + 1).getQuality()) {
+                    swap(j, j + 1);
+                }
+            }
+        }
+        sortedByAttribute = "quality";
+    }
+
+
+    private int BusquedaporPrecio(int elprecioabuscar) {
+        int izquierda = 0;
+        int derecha = data.size() - 1;
+        while (izquierda <= derecha) {
+            int centro = izquierda + (derecha - izquierda) / 2;
+            int actual = data.get(centro).getPrice();
+
+            if (actual == elprecioabuscar) {
                 return centro;
-            }else if(actual < lacalidad){
-                izquierda = centro+1;
-            }else{
-                derecha = centro-1;
+            } else if (actual > elprecioabuscar) {
+                derecha = centro - 1;
+            } else {
+                izquierda = centro + 1;
+            }
+        }
+        return -1; // no se encuentra en la lista
+    }
+
+    private int BusquedaporCategoria(String lacategoria) {
+        int izquierda = 0;
+        int derecha = data.size() - 1;
+        while (izquierda <= derecha) {
+            int centro = izquierda + (derecha - izquierda) / 2;
+            int actual = data.get(centro).getCategory().compareTo(lacategoria);
+            if (actual == 0) {
+                return centro;
+            } else if (actual < 0) {
+                izquierda = centro + 1;
+            } else {
+                derecha = centro - 1;
             }
         }
         return -1;
     }
 
-    private int BusquedaRangoprecio(int menorprecio , int mayorprecio){
-        int izquierda =0;
-        int derecha = data.size()-1;
-        int resultado =-1;
+    private int BusquedaporCalidad(int lacalidad) {
+        int izquierda = 0;
+        int derecha = data.size() - 1;
+        while (izquierda <= derecha) {
+            int centro = izquierda + (derecha - izquierda) / 2;
+            int actual = data.get(centro).getQuality();
 
-        while (izquierda <= derecha){
-            int centro = izquierda + (derecha - izquierda)/2;
+            if (actual == lacalidad) {
+                return centro;
+            } else if (actual < lacalidad) {
+                izquierda = centro + 1;
+            } else {
+                derecha = centro - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int BusquedaRangoprecio(int menorprecio, int mayorprecio) {
+        int izquierda = 0;
+        int derecha = data.size() - 1;
+        int resultado = -1;
+
+        while (izquierda <= derecha) {
+            int centro = izquierda + (derecha - izquierda) / 2;
             int actual = data.get(centro).getPrice();
 
-            if(actual  >= menorprecio && actual <= mayorprecio){
+            if (actual >= menorprecio && actual <= mayorprecio) {
                 resultado = centro;
-                derecha = centro-1;
-            }else if(actual < menorprecio ){
-                izquierda = centro+1;
-            }else {
-                derecha = centro-1;
+                derecha = centro - 1;
+            } else if (actual < menorprecio) {
+                izquierda = centro + 1;
+            } else {
+                derecha = centro - 1;
             }
         }
         return resultado;
     }
 
-    public ArrayList<Game> getGamesByPrice(int price){
+    public ArrayList<Game> getGamesByPrice(int price) {
         long timeI = System.nanoTime();
         ArrayList<Game> resultados = new ArrayList<>();
 
-        if (sortedByAttribute.equals("precio")){
+        if (sortedByAttribute.equals("precio")) {
             int indice = BusquedaporPrecio(price);
-            if (indice != -1){
+            if (indice != -1) {
                 resultados.add(data.get(indice));
 
                 int izquierda = indice;
 
-                int derecha = indice+1;
+                int derecha = indice + 1;
 
-                while (izquierda >= 0 && data.get(izquierda).getPrice() == price){
+                while (izquierda >= 0 && data.get(izquierda).getPrice() == price) {
                     izquierda--;
                 }
-                while (derecha < data.size() && data.get(derecha).getPrice() == price){
+                while (derecha < data.size() && data.get(derecha).getPrice() == price) {
                     resultados.add(data.get(derecha));
                     derecha++;
                 }
 
             }
-        //busqueda lienal si no estan ordenados
-        }else{
-          for (int i = 0; i < data.size(); i++){
-              if (data.get(i).getPrice() == price){
-                  resultados.add(data.get(i));
-              }
-          }
+            //busqueda lienal si no estan ordenados
+        } else {
+            for (int i = 0; i < data.size(); i++) {
+                if (data.get(i).getPrice() == price) {
+                    resultados.add(data.get(i));
+                }
+            }
         }
         long timeF = System.nanoTime();
         long duration = (timeF - timeI);
         return resultados;
     }
 
-    public ArrayList<Game> getGamesByPriceRange(int lowrPrice, int higherPrice){
+    public ArrayList<Game> getGamesByPriceRange(int lowrPrice, int higherPrice) {
         long timeI = System.nanoTime();
         ArrayList<Game> resultados = new ArrayList<>();
-        if (sortedByAttribute.equals("precio")){
-         int inicio = BusquedaRangoprecio(lowrPrice, higherPrice);
-         if (inicio != -1){
-             for (int i = inicio; i < data.size() && data.get(i).getPrice() <= higherPrice; i++){
-                 resultados.add(data.get(i));
-             }
-         }
-         //busqueda lienal
-        }else{
-            for (int i = 0; i < data.size(); i++){
+        if (sortedByAttribute.equals("precio")) {
+            int inicio = BusquedaRangoprecio(lowrPrice, higherPrice);
+            if (inicio != -1) {
+                for (int i = inicio; i < data.size() && data.get(i).getPrice() <= higherPrice; i++) {
+                    resultados.add(data.get(i));
+                }
+            }
+            //busqueda lienal
+        } else {
+            for (int i = 0; i < data.size(); i++) {
                 Game juego = data.get(i);
-                int precio= juego.getPrice();
-                if (precio >= lowrPrice && precio <= higherPrice){
+                int precio = juego.getPrice();
+                if (precio >= lowrPrice && precio <= higherPrice) {
                     resultados.add(juego);
                 }
             }
@@ -190,30 +226,30 @@ class Dataset{
         return resultados;
     }
 
-    public ArrayList<Game> getGamesByCategory(String category){
+    public ArrayList<Game> getGamesByCategory(String category) {
         long timeI = System.nanoTime();
         ArrayList<Game> resultados = new ArrayList<>();
 
-        if (sortedByAttribute.equals("category")){
+        if (sortedByAttribute.equals("category")) {
             int indice = BusquedaporCategoria(category);
-            if (indice != -1){
-             resultados.add(data.get(indice));
+            if (indice != -1) {
+                resultados.add(data.get(indice));
             }
 
-            int izquierda = indice-1;
-            int derecha = indice+1;
-            while (izquierda >= 0 && data.get(izquierda).getCategory().equals(category)){
+            int izquierda = indice - 1;
+            int derecha = indice + 1;
+            while (izquierda >= 0 && data.get(izquierda).getCategory().equals(category)) {
                 resultados.add(data.get(izquierda));
                 izquierda--;
             }
-            while (derecha < data.size() && data.get(derecha).getCategory().equals(category)){
+            while (derecha < data.size() && data.get(derecha).getCategory().equals(category)) {
                 resultados.add(data.get(derecha));
                 derecha++;
             }
 
-        }else {
-            for (int i = 0; i < data.size(); i++){
-                if (data.get(i).getCategory().equals(category)){
+        } else {
+            for (int i = 0; i < data.size(); i++) {
+                if (data.get(i).getCategory().equals(category)) {
                     resultados.add(data.get(i));
 
                 }
@@ -222,8 +258,7 @@ class Dataset{
         long timeF = System.nanoTime();
         long duration = (timeF - timeI);
         return resultados;
-}
+    }
 
- 
-
+    class
 }
